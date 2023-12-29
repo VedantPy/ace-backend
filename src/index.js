7816941a55fd9e061e985a245678374a943123ca
Vedant Pandey <vedantpandey434@gmail.com>
2023-12-29 19:41:56 +0530

    Add DB connection and update dev script

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require("dotenv").config({path: "./env"});
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../src/constants.js"
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({path: "./env"});
+connectDB();
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (err) => {
+//             console.log("Error: ", err)
+//             throw err
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.log("Error: ", error);
+//         throw error
+//     }
+// })()
